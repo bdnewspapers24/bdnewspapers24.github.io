293ff21c3967e05a1f645d499ac3b6ea0486db3d
shuvojit33 <shuvojit33@gmail.com>
2014-09-27 02:11:49 +0600

    kip

--- javascripts/main.js
+++ javascripts/main.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
   });
   
   $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 25;
+    var position = $($(this).attr("href")).offset().top - 30;
     $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
